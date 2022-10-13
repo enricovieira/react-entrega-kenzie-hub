eff4d06e3590183e91a59bd0f77193f71429f359
Enrico Vieira <enricovieira@hotmail.com>
2022-10-12 23:25:13 -0300

    feature: add login requisition

---
+++ react-entrega-kenzie-hub/src/Pages/NotFoundPage/index.jsx
@@ -0,0 +1,5 @@
+const NotFoundPage = () => {
+  return <h1>404 NOT FOUND</h1>;
+};
+
+export default NotFoundPage;
---
+++ react-entrega-kenzie-hub/src/Pages/RegisterPage/index.jsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+  return <h1>REGISTERPAGE</h1>;
+};
+
+export default RegisterPage;
